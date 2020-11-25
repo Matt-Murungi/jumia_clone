29ca33df65f4caacf413ac67e7f6bea24eea379e
Matt-Murungi <murungima@gmail.com>
2020-11-25 12:42:17 +0300

    add product-component product-heading

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import './Product.css'
+
 function Product() {
     return (
         <div className="product">
             <div className="product__heading">
                 <p className="product__headingTitle">Black Friday Deals | Up to 80% Off</p>
-                <p className="product__headingTitle">See All <span className="material-icons">keyboard_arrow_right</span> </p>
+                <div className="product__headingTitleMore">See All <span className="material-icons">keyboard_arrow_right</span> </div>
                 
             </div>
             
